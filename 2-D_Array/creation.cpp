97b81a9f7ba49dedd47c43f7de344175a6b70db9
Michaeldhiman <michaeldhiman0214@gmail.com>
2024-05-21 22:09:28 +0530

    update 2-D array

--- 2-D_Array/creation.cpp
+++ 2-D_Array/creation.cpp
@@ -16,14 +16,14 @@ int main(){
     cout<<"Row Wise Column"<<endl;
     for(int row=0;row<3;row++){
         for(int col=0;col<3;col++){
-            cout<<arr[col][row]<<" ";
+            cout<<arr[row][col]<<" ";
         }
         cout<<endl;
     }
     // columnWise;
     cout<<"Column Wise"<<endl;
-    for(int col=0;col<3;col++){
-        for(int row=0;row<3;row++){
+    for(int row=0;row<3;row++){
+        for(int col=0;col<3;col++){
             cout<<arr[col][row]<<" ";
         }
         cout<<endl;
@@ -42,5 +42,21 @@ int main(){
         }
         cout<<endl;
     }
+   // anti-diagonal Wise
+    cout<<"Anti-Diagonal Wise"<<endl;
+    for(int i=0;i<3;i++){
+            cout<<arr[i][3-i-1]<<" ";
+    }
+    // anti-diagonal Wise
+    cout<<"Anti-Diagonal Wise"<<endl;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(i+j==3-1){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
--- 2-D_Array/input_arr.cpp
+++ 2-D_Array/input_arr.cpp
@@ -2,20 +2,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int arr[2][3];
-    int row=2;
+    int arr[3][3];
+    int row=3;
     int column=3;
+    
     // input Row Wise
-    cout<<"Input Row wise"<<endl;
+
+
+    // cout<<"Input Row wise"<<endl;
+    // for(int i=0;i<row;i++){
+    //     for(int j=0;j<column;j++){
+    //         cout<<"Enter the Value for ("<<i<<","<<j<<")";
+    //         cin>>arr[i][j];
+    //     }
+    // }
+    // for(int i=0;i<row;i++){
+    //     for(int j=0;j<column;j++){
+    //         cout<<arr[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // input Column Wise
+    cout<<"Column Row wise"<<endl;
     for(int i=0;i<row;i++){
         for(int j=0;j<column;j++){
-            cout<<"Enter the Value for ("<<i<<","<<j<<")";
-            cin>>arr[i][j];
+            cout<<"Enter the Value for ("<<j<<","<<i<<")";
+            cin>>arr[j][i];
         }
     }
-    for(int i=0;i<row;i++){
-        for(int j=0;j<column;j++){
-            cout<<arr[i][j]<<" ";
+    for(int i=0;i<column;i++){
+        for(int j=0;j<row;j++){
+            cout<<arr[j][i]<<" ";
         }
         cout<<endl;
     }
