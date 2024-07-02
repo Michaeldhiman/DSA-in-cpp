648f00d212a6f720cd8a0066706a26612f4f69d5
Michaeldhiman <michaeldhiman0214@gmail.com>
2024-07-02 23:10:23 +0530

    Bank Class

--- Oops/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-    // void transfer(int amount,int myacc_no, customer& targetAccount){
-    //     if(withdrew(amount,myacc_no)){
-    //         targetAccount.deposit(amount,targetAccount.acc_no);
-    //     }else{
-    //          cout<<"Insufficient Amount"<<endl;
-    //     }
-    // }
