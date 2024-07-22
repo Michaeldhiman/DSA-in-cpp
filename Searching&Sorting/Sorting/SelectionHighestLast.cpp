fade0704928790c76c94ddef69a82fb3367634ea
Michaeldhiman <michaeldhiman0214@gmail.com>
2024-07-22 21:24:20 +0530

    sorting

---
+++ Searching&Sorting/Sorting/SelectionHighestLast.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void selection(int arr[],int size){
+    for(int i=size-1;i>0;i--){
+        int index=i;
+        for(int j=i-1;j>=0;j--){
+            if(arr[j]>arr[index]){
+                index=j;
+            }
+        }
+        swap(arr[i],arr[index]);
+    }
+    return ;
+}
+int main(){
+    int arr[]={65,25,12,22,23};
+    int size=5;
+    selection(arr,size);
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
